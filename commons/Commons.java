923ece4264fea91cc31910d762eb9f49bfd8c703
Shivam Kapoor <shivamkapoor23@gmail.com>
2016-02-15 15:37:06 +0530

    Commons used in project are in this file

--- commons/Commons.java
+++ commons/Commons.java
@@ -1,1 +1,8 @@
                                       /* Commons file */
+package commons;
+public class Constants
+{
+  public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/studentms"; // studentms is the database name
+  public static final String DB_USER_NAME = "root";
+  public static final String DB_PASSWORD = "shivam";
+}
